b408ebf09ee0ec2304b94ca30e7210d055c39f1c
almahfuz <almahfuz0327@gmail.com>
2022-11-08 13:52:10 +0600

    added mongodb, make services api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // dependencies
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config();
 
@@ -8,9 +9,32 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server running')
-})
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const servicesCollection = client.db('better-call-saul').collection('services');
+
+    // return only 3 services
+    app.get('/home', async (req, res) => {
+        // sort descendingly
+        const option = {
+            sort: {
+                "createdAt": -1
+            }
+        }
+        const cursor = servicesCollection.find({}, option);
+        const services = await cursor.limit(3).toArray();
+        console.log(services)
+        res.send(services);
+    })
+
+}
+run().catch(console.dir)
+
+
 app.listen(process.env.PORT, () => {
     console.log('Server running on', process.env.PORT)
 })
